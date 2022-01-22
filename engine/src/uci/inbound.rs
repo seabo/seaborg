0a4e02d761f8ebc36f15217c0a281b16b6caa99b
seabo <37074155+seabo@users.noreply.github.com>
2022-01-22 13:29:34 +0000

    Implement logging of inbound UCI commands

--- engine/src/uci/inbound.rs
+++ engine/src/uci/inbound.rs
@@ -1,13 +1,17 @@
 use super::{Req, Uci};
-use std::io;
+use std::fs;
+use std::io::{self, Write};
 
+// TODO: this doesn't make sense as part of UCI. Reading from the command
+// line should go elsewhere.
 impl Uci {
     /// Blocking method which awaits the next command to stdin.
     pub fn read_command() -> Req {
         let mut buf = String::new();
 
-        // TODO: use the `Result`.
-        io::stdin().read_line(&mut buf);
+        io::stdin().read_line(&mut buf).expect("couldn't read line");
+
+        Uci::log(&buf);
 
         match Uci::parse(buf) {
             Ok(msg) => msg,
@@ -16,5 +20,16 @@ impl Uci {
                 Self::read_command()
             }
         }
+    }
+
+    fn log(buf: &str) {
+        let path = "../../log.txt";
+        let mut file = fs::OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(path)
+            .expect("Unable to open file");
+        file.write_all(buf.as_bytes())
+            .expect("Unable to write data");
     }
 }
