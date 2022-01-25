08e7dc163ecc530bf430354e3e02b0fa8315c5c5
seabo <37074155+seabo@users.noreply.github.com>
2022-01-25 18:20:39 +0000

    Remove unused search stuff

--- engine/src/search/mod.rs
+++ engine/src/search/mod.rs
@@ -1,8 +1,4 @@
-pub mod alphabeta;
-pub mod negamax;
 pub mod ordering;
 pub mod params;
 pub mod perft;
-pub mod perft_with_tt;
-pub mod pv_search;
 pub mod search;
--- src/dev.rs
+++ src/dev.rs
@@ -1,10 +1,9 @@
 use core::init::init_globals;
 use core::position::Position;
 use engine::eval::material_eval;
-use engine::search::params::{Builder, Params};
+use engine::search::ordering::OrderedMoveList;
+use engine::search::params::Builder;
 use engine::search::perft::Perft;
-use engine::search::perft_with_tt::PerftWithTT;
-use engine::search::pv_search::{OrderedMoveList, PVSearch};
 use engine::search::search::Search;
 use engine::uci::Pos;
 
@@ -16,7 +15,6 @@ use std::time::Instant;
 pub fn dev() {
     init_globals();
     do_quiesce();
-    // do_perft_with_tt();
     // do_transpo_table();
     // do_zobrist();
     // do_perft();
@@ -39,35 +37,6 @@ fn do_quiesce() {
     println!("Quiescent eval: {}", search.quiesce(-10_000, 10_000));
 }
 
-fn do_perft_with_tt() {
-    let mut pos = Position::start_pos();
-    let depth = 7;
-    let now = Instant::now();
-    let nodes = PerftWithTT::perft(&mut pos, depth);
-    let elapsed = now.elapsed();
-    println!("Nodes: {}", nodes.separated_string());
-    println!(
-        "{}µs to calculate perft {}",
-        elapsed.as_micros().separated_string(),
-        depth
-    );
-
-    let now = Instant::now();
-    let perft_result = Perft::divide(&mut pos, depth, false);
-    let elapsed = now.elapsed();
-
-    println!(
-        "{}µs to calculate perft {}",
-        elapsed.as_micros().separated_string(),
-        depth
-    );
-    println!(
-        "{} nodes/sec",
-        ((perft_result.nodes.unwrap() * 1_000_000_000) / (elapsed.as_nanos() as usize))
-            .separated_string()
-    );
-}
-
 fn do_zobrist() {
     let mut pos = Position::start_pos();
     println!("Startpos: {}", pos.zobrist());
@@ -83,45 +52,6 @@ fn do_material_eval() {
     match pos {
         Ok(ref mut pos) => {
             println!("Material eval: {}", material_eval(&pos));
-        }
-        Err(fen_error) => {
-            println!("{}", fen_error.msg);
-        }
-    }
-}
-
-fn do_pv_search() {
-    let mate_in_1 = "8/p1p2ppk/4p1q1/8/1B5p/P4QbP/1P4PK/3r4 w - - 6 33";
-    let mate_in_3 = "r5rk/5p1p/5R2/4B3/8/8/7P/7K w - - 0 1";
-    let bernstein_kotov = "R6R/1r3pp1/4p1kp/3pP3/1r2qPP1/7P/1P1Q3K/8 w - - 0 1";
-    let grau_colle = "1k5r/pP3ppp/3p2b1/1BN1n3/1Q2P3/P1B5/KP3P1P/7q w - - 0 1";
-    let mate_with_promo = "8/R7/4kPP1/3ppp2/3B1P2/1K1P1P2/8/8 w - - 0 1";
-    let mate_in_5 = "4b3/4B1bq/p2Q2pp/4pp2/8/8/p7/k1K5 w - - 0 1";
-    let random_pos = "5k2/p1p2pp1/2Q1p1q1/8/7p/P1B4P/1P3bPK/3r4 w - - 0 30";
-    let skewer = "4q3/8/8/8/4k3/8/1K6/6Q1 w - - 0 1";
-    let mate_in_7 = "rn3rk1/pbppq1pp/1p2pb2/4N2Q/3PN3/3B4/PPP2PPP/R3K2R w KQ - 7 11";
-
-    let pos = Position::from_fen(mate_in_7);
-
-    match pos {
-        Ok(pos) => {
-            let turn = pos.turn().clone();
-            let now = Instant::now();
-            let mut searcher = PVSearch::new(pos);
-            // let val = searcher.pv_search(9, -10000, 10000) * if turn.is_white() { 1 } else { -1 };
-            let val = searcher.iterative_deepening(19) * if turn.is_white() { 1 } else { -1 };
-            let elapsed = now.elapsed();
-            let pv = searcher.recover_pv();
-            for mov in pv {
-                print!("{} ", mov);
-            }
-
-            println!("{}", val);
-            println!(
-                "Evaluated position in {}ms",
-                elapsed.as_millis().separated_string()
-            );
-            searcher.display_trace();
         }
         Err(fen_error) => {
             println!("{}", fen_error.msg);
